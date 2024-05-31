4c9ee272b4b5548f397310edda69e9b04ae1489c
raihandff <daffa.raihanbtk@gmail.com>
2024-05-31 16:36:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,36 +33,6 @@
         }
     </style> 
         <?php include "navbar.html" ?>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">TUBAN</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="7. Pengaduan.php">Pengaduan</a>
-                                <a class="dropdown-item" href="3. INFORMASIRUANGBERITA.php">Ruang Berita</a>
-                                <a class="dropdown-item" href="6. LacakPengaduan.php">Lacak Pengaduan</a>
-                                <a class="dropdown-item" href="4. STATISTIK.php">Statistik & Analisis</a>
-                                <a class="dropdown-item" href="2. GALERIPENGADUAN.php">Galeri Pengaduan</a>
-                                <a class="dropdown-item" href="INTANSINEGARA.php">Intansi Negara</a>
-                                <a class="dropdown-item" href="KATEGORIPENGADUAN.php">Kategori Pengaduan</a>
-                            </div>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="About.php">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="notification.html">Notifikasi</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Profile</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>   
     <header class="masthead">
         <div class="container h-100">
             <div class="row h-100 align-items-end" style="padding-bottom: 150px;">
